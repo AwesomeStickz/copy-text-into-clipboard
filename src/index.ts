e39603ccb88466934f3ed7af25bc28a7c93afe95
Awesome Stickz <kiran121102@gmail.com>
2020-10-31 18:48:31 +0530

    Add options.copyFromFile to copy contents of a file

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import * as childProcess from 'child_process';
+import * as path from 'path';
 
-export const copy = (text: string) => {
+export const copy = (text: string, options?: { copyFromFile?: boolean }) => {
     if (typeof text !== 'string') return console.error(new TypeError('Type of text must be a string!'));
     if (process.platform !== 'win32') return console.error(new Error('This package works only in windows currently!'));
 
+    if (options?.copyFromFile === true) {
+        const filePath = path.resolve(text);
 
-    childProcess.exec(`(echo ${text.replace(/\n/g, '& echo ')}) | clip`);
+        childProcess.exec(`clip < ${filePath.replace('\\', '\\"')}`);
+    } else {
+        childProcess.exec(`(echo ${text.replace(/\n/g, '& echo ')}) | clip`);
+    }
 };
