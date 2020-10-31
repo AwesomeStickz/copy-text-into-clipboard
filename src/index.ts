de18313c51dab5df3549f62d2cf00e88231d66d2
Awesome Stickz <kiran121102@gmail.com>
2020-10-31 18:45:52 +0530

    Use console.error() instead of throw to show file path

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as childProcess from 'child_process';
 
 export const copy = (text: string) => {
-    if (typeof text !== 'string') throw new Error('Type of text must be a string!');
-    if (process.platform !== 'win32') throw new Error('This package works only in windows currently!');
+    if (typeof text !== 'string') return console.error(new TypeError('Type of text must be a string!'));
+    if (process.platform !== 'win32') return console.error(new Error('This package works only in windows currently!'));
+
 
     childProcess.exec(`(echo ${text.replace(/\n/g, '& echo ')}) | clip`);
 };
